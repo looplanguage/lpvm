bda9eea89bb57abed1919c8d6ad87171dd38c10f
Kanersps <kanepetra@gmail.com>
2021-09-16 22:17:24 +0200

    feature: repl for testing

--- go.mod
+++ go.mod
@@ -2,7 +2,16 @@ module github.com/looplanguage/lpvm
 
 go 1.17
 
-require github.com/looplanguage/loop v0.1.0 // indirect
-require github.com/looplanguage/compiler v0.1.0
+require (
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+)
+
+require (
+	github.com/fatih/color v1.12.0
+	github.com/looplanguage/compiler v0.1.0
+	github.com/looplanguage/loop v0.1.0
+)
 
 replace github.com/looplanguage/compiler => ../compiler
---
+++ repl/repl.go
@@ -0,0 +1,63 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/looplanguage/compiler/compiler"
+	"github.com/looplanguage/loop/lexer"
+	"github.com/looplanguage/loop/parser"
+	"github.com/looplanguage/lpvm/vm"
+	"io"
+	"runtime"
+)
+
+// TODO: For testing, remove in eventual build & replace with it's own executable.
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		if runtime.GOOS != "windows" {
+			io.WriteString(out, "\\033[31m#\\033[0m")
+		} else {
+			io.WriteString(out, "# ")
+		}
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.Create(line)
+		p := parser.Create(l)
+
+		program := p.Parse()
+		if len(p.Errors) != 0 {
+			for _, e := range p.Errors {
+				fmt.Println(e)
+			}
+			continue
+		}
+
+		comp := compiler.Create()
+		err := comp.Compile(program)
+
+		if err != nil {
+			fmt.Fprintf(out, "Compilation failed. \n%s\n", err)
+			continue
+		}
+
+		machine := vm.Create(comp.Bytecode())
+
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "vm failed running bytecode with: \n%s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
