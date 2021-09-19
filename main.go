d31d8acafd248a24835617e07714ca5b060084c6
Kanersps <kanepetra@gmail.com>
2021-09-19 19:30:57 +0200

    feat: allow running executable files

--- main.go
+++ main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"github.com/looplanguage/compiler/compiler"
 	"github.com/looplanguage/lpvm/repl"
+	"github.com/looplanguage/lpvm/vm"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
-	repl.Start(os.Stdin, os.Stdout)
+	if len(os.Args) != 2 {
+		repl.Start(os.Stdin, os.Stdout)
+		return
+	}
+
+	compiler.RegisterGobTypes()
+	bts, err := ioutil.ReadFile(os.Args[1])
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var constantBytes bytes.Buffer
+	constantBytes.Write(bts)
+
+	dec := gob.NewDecoder(&constantBytes)
+	var Bytecode compiler.Bytecode
+	err = dec.Decode(&Bytecode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	machine := vm.Create(&Bytecode)
+	err = machine.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if machine.LastPoppedStackElem() != nil {
+		fmt.Println(machine.LastPoppedStackElem().Inspect())
+	}
 }
